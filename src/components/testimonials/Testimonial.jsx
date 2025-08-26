bef9bae92ad4f9193efedea249f9dd6460f0110e
aarushgupta1 <88345028+aarushgupta1@users.noreply.github.com>
2025-08-26 09:17:57 -0500

    Updates

--- src/components/testimonials/Testimonial.jsx
+++
@@ -1,9 +0,0 @@
-export default function Testimonial(props) {
-    return (
-            <swiper-slide className="testimonial">
-                <p className="review"><strong className="quotes">"</strong>{props.testimonial.review}<strong className="quotes">"</strong></p>
-                <p className="rating"><span role="img" aria-label="rating">{props.testimonial.rating}</span></p>
-                <p className="reviewer flex-element"><i>{props.testimonial.reviewer}</i></p>
-            </swiper-slide>
-    )
-}
